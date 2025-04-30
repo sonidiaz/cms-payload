7cdeb7b60c84c968af85b00cba1efd1443894b90
Nacho Díaz <nachodiaz8@gmail.com>
2025-04-30 13:58:37 +0200

    update seo title page

--- src/utilities/generateMeta.ts
+++ src/utilities/generateMeta.ts
@@ -27,8 +27,8 @@ export const generateMeta = async (args: {
   const ogImage = getImageURL(doc?.meta?.image)
 
   const title = doc?.meta?.title
-    ? doc?.meta?.title + ' | Payload Website Template'
-    : 'Payload Website Template'
+    ? doc?.meta?.title + ' | Cociñando a alimentación futura'
+    : 'Cociñando a alimentación futura'
 
   return {
     description: doc?.meta?.description,
